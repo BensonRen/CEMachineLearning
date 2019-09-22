f2f50b85e5d076fd3fa647522498637d390e32dc
Ben <rensimiao.ben@gmail.com>
2019-09-22 19:30:06 -0400

    making functions and separting modules

--- Lab2/lenet5_cifar10.py
+++ Lab2/lenet5_cifar10.py
@@ -17,46 +17,10 @@ import flag_reader
 
 # You cannot change this line.
 from tools.dataloader import CIFAR10
-
-if __name__ == '__main__':
-    flags = flag_reader.read_flag()
-
-    print("data acquiring and preprocessing...")
-    transform_train = transforms.Compose([transforms.ToTensor(),
-                                         transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010)),
-                                         transforms.RandomCrop(32, padding = 4),
-                                         transforms.RandomHorizontalFlip()])
-                                         #transforms.RandomChoice([
-                                         #transforms.Grayscale(num_output_channels = 3),
-                                         #transforms.RandomCrop(224),
-                                         #transforms.RandomVerticalFlip(0.5),
-                                         #transforms.RandomHorizontalFlip(0.5)])])
-
-    transform_val = transform_train = transforms.Compose([transforms.ToTensor(),
-                                         transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010)),
-                                         ])
-
-
-    # Call the dataset Loader
-    trainset = CIFAR10(root=flags.dataroot, train=True, download=True, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=flags.train_batch_size, shuffle=True, num_workers=1)
-    valset = CIFAR10(root=flags.dataroot, train=False, download=True, transform=transform_val)
-    valloader = torch.utils.data.DataLoader(valset, batch_size=flags.val_batch_size, shuffle=False, num_workers=1)
-
-    print("Building the model and network")
-
-    # Specify the device for computation
-    device = flags.device if torch.cuda.is_available() else 'cpu'
-    net = LeNet5()
-    net = net.to(device)
-    if device =='cuda':
-        print("Train on GPU...")
-    else:
-        print("Train on CPU...")
-
-    print("Getting the pre-trained model / train from scratch")
+def recoverfromckpt():
     # Code for loading checkpoint and recover epoch id.
     CKPT_PATH = "./saved_model/model.h5"
+
     def get_checkpoint(ckpt_path):
         try:
             ckpt = torch.load(ckpt_path)
@@ -73,31 +37,45 @@ if __name__ == '__main__':
         start_epoch = 0
         current_learning_rate = flags.initial_lr
     else:
-        print("Successfully loaded checkpoint: %s" %CKPT_PATH)
+        print("Successfully loaded checkpoint: %s" % CKPT_PATH)
         net.load_state_dict(ckpt['net'])
         start_epoch = ckpt['epoch'] + 1
         current_learning_rate = ckpt['lr']
-        print("Starting from epoch %d " %start_epoch)
+        print("Starting from epoch %d " % start_epoch)
 
-    print("Starting from learning rate %f:" %current_learning_rate)
+    return start_epoch,current_learning_rate
 
-    print('Setting up the loss function...')
-    # Create loss function and specify regularization
-    criterion = nn.CrossEntropyLoss() #L2 norminat weight_decay for optimizer
-    # Add optimizer
-    optimizer = torch.optim.SGD(net.parameters(), lr= flags.initial_lr, momentum = flags.momentum, weight_decay=flags.reg)
 
-# Start the training/validation process
-# The process should take about 5 minutes on a GTX 1070-Ti
-# if the code is written efficiently.
-global_step = 0
-best_val_acc = 0
+def gettraintest():
+    transform_train = transforms.Compose([transforms.ToTensor(),
+                                          transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                                          transforms.RandomCrop(32, padding=4),
+                                          transforms.RandomHorizontalFlip()])
+    # transforms.RandomChoice([
+    # transforms.Grayscale(num_output_channels = 3),
+    # transforms.RandomCrop(224),
+    # transforms.RandomVerticalFlip(0.5),
+    # transforms.RandomHorizontalFlip(0.5)])])
 
-for i in range(start_epoch, flags.epochs):
+    transform_val = transform_train = transforms.Compose([transforms.ToTensor(),
+                                                          transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                                                               (0.2023, 0.1994, 0.2010)),
+                                                          ])
+
+    # Call the dataset Loader
+    trainset = CIFAR10(root=flags.dataroot, train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=flags.train_batch_size, shuffle=True, num_workers=1)
+    valset = CIFAR10(root=flags.dataroot, train=False, download=True, transform=transform_val)
+    valloader = torch.utils.data.DataLoader(valset, batch_size=flags.val_batch_size, shuffle=False, num_workers=1)
+
+    return trainloader,valloader
+
+
+def train_one_epoch(i):
     print(datetime.datetime.now())
     # Switch to train mode
     net.train()
-    print("Epoch %d:" %i)
+    print("Epoch %d:" % i)
 
     total_examples = 0
     correct_examples = 0
@@ -122,7 +100,7 @@ for i in range(start_epoch, flags.epochs):
         # Calculate predicted labels
         _, predicted = outputs.max(1)
         # Calculate accuracy
-        #print((predicted == targets).sum().item())
+        # print((predicted == targets).sum().item())
         total_examples += list(inputs.size())[0]
         correct_examples += (predicted == targets).sum().item()
 
@@ -130,16 +108,16 @@ for i in range(start_epoch, flags.epochs):
 
         global_step += 1
         if global_step % 100 == 0:
-            avg_loss = train_loss / (batch_idx + 1)
+            train_avg_loss = train_loss / (batch_idx + 1)
         pass
-    avg_acc = correct_examples / total_examples
-    print("Training loss: %.4f, Training accuracy: %.4f" %(avg_loss, avg_acc))
+    train_avg_acc = correct_examples / total_examples
+    print("Training loss: %.4f, Training accuracy: %.4f" % (train_avg_loss, train_avg_acc))
     print(datetime.datetime.now())
     # Validate on the validation dataset
     print("Validation...")
     total_examples = 0
     correct_examples = 0
-    
+
     net.eval()
 
     val_loss = 0
@@ -159,13 +137,66 @@ for i in range(start_epoch, flags.epochs):
             _, predicted = outputs.max(1)
             # Calculate accuracy
             total_examples += list(inputs.size())[0]
-            correct_examples +=  (predicted == targets).sum().item()
+            correct_examples += (predicted == targets).sum().item()
             val_loss += loss
 
-    avg_loss = val_loss / len(valloader)
-    avg_acc = correct_examples / total_examples
-    print("Validation loss: %.4f, Validation accuracy: %.4f" % (avg_loss, avg_acc))
+    val_avg_loss = val_loss / len(valloader)
+    val_avg_acc = correct_examples / total_examples
+    print("Validation loss: %.4f, Validation accuracy: %.4f" % (val_avg_loss, val_avg_acc))
 
+
+    return train_avg_loss, train_avg_acc, val_avg_loss, val_avg_acc
+
+
+
+if __name__ == '__main__':
+    flags = flag_reader.read_flag()
+
+    print("data acquiring and preprocessing...")
+    trainloader,valloader = gettraintest()
+    print("Building the model and network")
+
+    # Specify the device for computation
+    device = flags.device if torch.cuda.is_available() else 'cpu'
+    net = LeNet5()
+    net = net.to(device)
+    if device =='cuda':
+        print("Train on GPU...")
+    else:
+        print("Train on CPU...")
+
+    print("Getting the pre-trained model / train from scratch")
+    start_epoch,current_learning_rate = recoverfromckpt()
+
+    print("Starting from learning rate %f:" %current_learning_rate)
+
+    print('Setting up the loss function...')
+    # Create loss function and specify regularization
+    criterion = nn.CrossEntropyLoss() #L2 norminat weight_decay for optimizer
+    # Add optimizer
+    optimizer = torch.optim.SGD(net.parameters(), lr= flags.initial_lr, momentum = flags.momentum, weight_decay=flags.reg)
+
+# Start the training/validation process
+# The process should take about 5 minutes on a GTX 1070-Ti
+# if the code is written efficiently.
+global_step = 0
+best_val_acc = 0
+#Initialize the lists for training and validations
+#train_avg_loss_list, train_avg_acc_list, val_avg_loss_list, val_avg_acc_list = [],[],[],[]
+train_loss_acc_val_loss_acc = [[],[],[],[]]
+for i in range(start_epoch, flags.epochs):
+    train_avg_loss, train_avg_acc, val_avg_loss, val_avg_acc = train_one_epoch(i)
+    train_loss_acc_val_loss_acc[0].append(train_avg_loss)
+    train_loss_acc_val_loss_acc[1].append(train_avg_acc)
+    train_loss_acc_val_loss_acc[2].append(val_avg_loss)
+    train_loss_acc_val_loss_acc[3].append(val_avg_acc)
+
+dirname = os.path.join(ckpt_dir, time.strftime('%Y%m%d_%H%M%S', time.localtime()))
+os.mkdir(dirname)
+for i in range(4):
+    np.savetxt(os.path.join(dirname,'{}.txt'.format(i)), train_loss_acc_val_loss_acc[i],delimiter=',')
+
+    
 """
     Assignment 4(b)
     Learning rate is an important hyperparameter to tune. Specify a 
@@ -203,3 +234,5 @@ for i in range(start_epoch, flags.epochs):
 
 print("Optimization finished.")
 """
+
+
